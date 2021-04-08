a4376e40f765534e1db6d98f962b7c03d0916cb1
jinyinMAC <crystaljinyin@qq.com>
2021-04-08 18:56:31 +0800

    将压缩时间和压缩对象大小对比写出到文件

--- src/main/java/net/csibio/aird/stackData2Rep.java
+++ src/main/java/net/csibio/aird/stackData2Rep.java
@@ -12,7 +12,7 @@ public class stackData2Rep {
         for (int k = 10; k < 11; k++) {
             int arrNum = (int) Math.pow(2, k);
             //生成有序数组
-            List<int[]> arrGroup = new LinkedList<>();
+            List<int[]> arrGroup = new ArrayList<>();
             for (int i = 0; i < arrNum; i++) {
                 int[] arr = new int[3000 + (int) (Math.random() * 100)];//模拟一张光谱3000多个m/z
                 arr[0] = 1000000;//每个mz是百万级的整数
@@ -21,20 +21,26 @@ public class stackData2Rep {
                 }
                 arrGroup.add(arr);
             }
-            Stack stack = stackEncode(arrGroup);
-            long t = System.currentTimeMillis();
-            List<int[]> stackDecode = stackDecode(stack);
-//            List<int[]> stackDecodeAbort = stackDecodeAbort(stack);
 
-            System.out.println("解压时间：" + (System.currentTimeMillis() - t));
-//            boolean a = Boolean.TRUE;
-//            for (int i = 0; i < arrGroup.size(); i++) {
-//                a = Arrays.equals(arrGroup.get(i), stackDecode.get(i));
-//                if (!a) {
-//                    break;
-//                }
-//            }
-//            System.out.println("对照压缩前后数组是否相同" + a);
+            long t1 =System.currentTimeMillis();
+            Stack stack = stackEncode(arrGroup);
+            System.out.println("压缩时间：" + (System.currentTimeMillis() - t1));
+            System.out.println();
+
+            long t2 = System.currentTimeMillis();
+//            List<int[]> stackDecodeAbort = stackDecodeAbort(stack);
+            List<int[]> stackDecode = stackDecode(stack);
+            System.out.println("解压时间：" + (System.currentTimeMillis() - t2));
+            System.out.println();
+
+            boolean a = Boolean.TRUE;
+            for (int i = 0; i < arrGroup.size(); i++) {
+                a = Arrays.equals(arrGroup.get(i), stackDecode.get(i));
+                if (!a) {
+                    break;
+                }
+            }
+            System.out.println("对照压缩前后数组是否相同" + a);
         }
     }
 
@@ -46,19 +52,12 @@ public class stackData2Rep {
         }
 
         //合并排序数组
-        long t = System.currentTimeMillis();
-        List<int[]> tempArrGroup = new ArrayList<>();
-        for (int i = 0; i < arrGroup.size(); i++) {
-            tempArrGroup.add(Arrays.copyOf(arrGroup.get(i), arrGroup.get(i).length));
-        }
         int[][] stackSort;
         if (pair) {
-            stackSort = getPairSortArray(tempArrGroup);
+            stackSort = getPairSortArray(arrGroup);
         } else {
-            stackSort = getQueueSortArray(tempArrGroup);
+            stackSort = getQueueSortArray(arrGroup);
         }
-//        System.out.println("合并排序数组时间:" + (System.currentTimeMillis() - t));
-
         //取出stack数组和index数组
         int[] stackArr = new int[stackLen];
         int[] stackIndex = new int[stackLen];
@@ -66,9 +65,7 @@ public class stackData2Rep {
             stackArr[i] = stackSort[i][0];
             stackIndex[i] = stackSort[i][1];
         }
-
         //index移位存储
-        long t0 = System.currentTimeMillis();
         int digit = (int) Math.ceil(Math.log(arrGroup.size()) / Math.log(2));
         int indexLen = (stackLen * digit - 1) / 8 + 1;
         byte[] value = new byte[8 * indexLen];
@@ -78,7 +75,6 @@ public class stackData2Rep {
                 value[fromIndex + j] = (byte) ((stackIndex[i] >> j) & 1);
             }
         }
-
         //把8个byte并为1个byte，用byte数组存是因为zlib压缩的是byte
         byte[] indexShift = new byte[indexLen];
         for (int i = 0; i < indexLen; i++) {
@@ -88,15 +84,9 @@ public class stackData2Rep {
                 indexShift[i] = (byte) temp;
             }
         }
-//        System.out.println("移位时间:" + (System.currentTimeMillis() - t0));
-
         //数组用fastPFor压缩，index用zlib压缩，并记录层数
         Stack stack = new Stack();
-
-        long t1 = System.currentTimeMillis();
         stack.comArr = CompressUtil.transToByte(CompressUtil.fastPforEncoder(stackArr));
-//        System.out.println("Pfor时间：" + (System.currentTimeMillis() - t1));
-
         stack.comIndex = CompressUtil.zlibEncoder(indexShift);
         stack.digit = digit;
         return stack;
@@ -118,7 +108,7 @@ public class stackData2Rep {
 //        int[][] stackSort = getFullSortArray(tempArrGroup);
 //        int[][] stackSort = getQueueSortArray(tempArrGroup);
         int[][] stackSort = getPairSortArray(tempArrGroup);
-//        System.out.println("合并排序数组时间:" + (System.currentTimeMillis() - t));
+        System.out.println("合并排序数组时间:" + (System.currentTimeMillis() - t));
 
         //取出stack数组和index数组
         int[] stackArr = new int[stackSort.length];
@@ -149,109 +139,45 @@ public class stackData2Rep {
                 indexShift[i] = (byte) temp;
             }
         }
-//        System.out.println("移位时间:" + (System.currentTimeMillis() - t0));
+        System.out.println("索引移位时间:" + (System.currentTimeMillis() - t0));
 
         //数组用fastPFor压缩，index用zlib压缩，并记录层数
         Stack stack = new Stack();
-
         long t1 = System.currentTimeMillis();
         stack.comArr = CompressUtil.transToByte(CompressUtil.fastPforEncoder(stackArr));
-//        System.out.println("Pfor时间：" + (System.currentTimeMillis() - t1));
-
+        System.out.println("zlib+Pfor压缩数组时间：" + (System.currentTimeMillis() - t1));
+        long t2 =System.currentTimeMillis();
         stack.comIndex = CompressUtil.zlibEncoder(indexShift);
+        System.out.println("zlib压缩索引时间：" + (System.currentTimeMillis() - t2));
         stack.digit = digit;
         return stack;
     }
 
-
-    public static List<int[]> stackDecodeAbort(Stack stack) {
-        int[] stackArr = CompressUtil.fastPforDecoder(CompressUtil.transToInteger(stack.getComArr()));
-        int[] stackIndex = new int[stackArr.length];
-        byte[] indexShift = CompressUtil.zlibDecoder(stack.getComIndex());
-        int digit = stack.getDigit();
-
-        //拆分byte为8个bit，并分别存储
-//        long t0 = System.currentTimeMillis();
-        byte[] value = new byte[8 * indexShift.length];
-        for (int i = 0; i < indexShift.length; i++) {
-            for (int j = 0; j < 8; j++) {
-                value[8 * i + j] = (byte) (((indexShift[i] & 0xff) >> j) & 1);
-            }
-        }
-//        System.out.println("拆分时间:" + (System.currentTimeMillis() - t0));
-
-        //还原为int类型的index
-//        long t1 = System.currentTimeMillis();
-        for (int i = 0; i < stackIndex.length; i++) {
-            for (int j = 0; j < digit; j++) {
-                stackIndex[i] += value[digit * i + j] << j;
-            }
-        }
-//        System.out.println("移位时间:" + (System.currentTimeMillis() - t1));
-
-        //合并数组和索引为一个二维数组
-        long t2 = System.currentTimeMillis();
-        int[][] stackSort = new int[stackArr.length][2];
-        for (int i = 0; i < stackArr.length; i++) {
-            stackSort[i][0] = stackArr[i];
-            stackSort[i][1] = stackIndex[i];
-        }
-        Arrays.sort(stackSort, (a, b) -> (a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]));
-        System.out.println("合并排序时间：" + (System.currentTimeMillis() - t2));
-
-        //统计index数组中各个元素出现的次数
-        long t3 = System.currentTimeMillis();
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int index : stackIndex) {
-            map.merge(index, 1, Integer::sum);
-        }
-        System.out.println("统计长度时间：" + (System.currentTimeMillis() - t3));
-        System.out.println(
-                stackIndex.length
-        );
-
-//        int maxIndex = stackSort[stackSort.length-1][1];
-
-        //根据index拆分stackArr,还原数组
-//        long t4 = System.currentTimeMillis();
-        List<int[]> arrGroup = new LinkedList<>();
-        int fromIndex = 0;
-        for (int i = 0; i < stackSort[stackSort.length - 1][1] + 1; i++) {
-            int[] arr = new int[map.get(i)];
-            for (int j = 0; j < map.get(i); j++) {
-                arr[j] = stackSort[fromIndex + j][0];
-            }
-            fromIndex += map.get(i);
-            arrGroup.add(arr);
-        }
-//        System.out.println("拆分数组时间："+(System.currentTimeMillis() - t4));
-        return arrGroup;
-    }
-
     public static List<int[]> stackDecode(Stack stack) {
+        long t1 = System.currentTimeMillis();
         int[] stackArr = CompressUtil.fastPforDecoder(CompressUtil.transToInteger(stack.getComArr()));
+        System.out.println("pfor+zlib解压数组时间：" + (System.currentTimeMillis() - t1));
         int[] stackIndex = new int[stackArr.length];
+        long t2 = System.currentTimeMillis();
         byte[] indexShift = CompressUtil.zlibDecoder(stack.getComIndex());
+        System.out.println("zlib索引解压时间：" + (System.currentTimeMillis() - t2));
         int digit = stack.getDigit();
 
         //拆分byte为8个bit，并分别存储
-        long t0 = System.currentTimeMillis();
+        long t = System.currentTimeMillis();
         byte[] value = new byte[8 * indexShift.length];
         for (int i = 0; i < indexShift.length; i++) {
             for (int j = 0; j < 8; j++) {
                 value[8 * i + j] = (byte) (((indexShift[i] & 0xff) >> j) & 1);
             }
         }
-        System.out.println("拆分时间:" + (System.currentTimeMillis() - t0));
-
         //还原为int类型的index
-        long t1 = System.currentTimeMillis();
         for (int i = 0; i < stackIndex.length; i++) {
             for (int j = 0; j < digit; j++) {
                 stackIndex[i] += value[digit * i + j] << j;
             }
         }
-        System.out.println("移位时间:" + (System.currentTimeMillis() - t1));
+        System.out.println("还原index时间:" + (System.currentTimeMillis() - t));
 
         //统计index数组中各个元素出现的次数
         long t3 = System.currentTimeMillis();
@@ -260,9 +186,6 @@ public class stackData2Rep {
             map.merge(index, 1, Integer::sum);
         }
         System.out.println("统计长度时间：" + (System.currentTimeMillis() - t3));
-        System.out.println(
-                stackIndex.length
-        );
 
         //根据index拆分stackArr,还原数组
         long t4 = System.currentTimeMillis();
@@ -275,30 +198,90 @@ public class stackData2Rep {
         for (int i = 0; i < stackIndex.length; i++) {
             arrGroup.get(stackIndex[i])[p[stackIndex[i]]++] = stackArr[i];
         }
-        System.out.println("拆分数组时间："+(System.currentTimeMillis() - t4));
+        System.out.println("拆分数组时间：" + (System.currentTimeMillis() - t4));
         return arrGroup;
     }
 
+//    public static List<int[]> stackDecodeAbort(Stack stack) {
+//        int[] stackArr = CompressUtil.fastPforDecoder(CompressUtil.transToInteger(CompressUtil.zlibDecoder(stack.getComArr())));
+//        int[] stackIndex = new int[stackArr.length];
+//        byte[] indexShift = CompressUtil.zlibDecoder(stack.getComIndex());
+//        int digit = stack.getDigit();
+//
+//        //拆分byte为8个bit，并分别存储
+////        long t0 = System.currentTimeMillis();
+//        byte[] value = new byte[8 * indexShift.length];
+//        for (int i = 0; i < indexShift.length; i++) {
+//            for (int j = 0; j < 8; j++) {
+//                value[8 * i + j] = (byte) (((indexShift[i] & 0xff) >> j) & 1);
+//            }
+//        }
+////        System.out.println("拆分时间:" + (System.currentTimeMillis() - t0));
+//
+//        //还原为int类型的index
+////        long t1 = System.currentTimeMillis();
+//        for (int i = 0; i < stackIndex.length; i++) {
+//            for (int j = 0; j < digit; j++) {
+//                stackIndex[i] += value[digit * i + j] << j;
+//            }
+//        }
+////        System.out.println("移位时间:" + (System.currentTimeMillis() - t1));
+//
+//        //统计index数组中各个元素出现的次数
+//        long t3 = System.currentTimeMillis();
+//        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+//        for (int index : stackIndex) {
+//            map.merge(index, 1, Integer::sum);
+//        }
+//        System.out.println("统计长度时间：" + (System.currentTimeMillis() - t3));
+//
+//        //合并数组和索引为一个二维数组
+//        long t2 = System.currentTimeMillis();
+//        int[][] stackSort = new int[stackArr.length][2];
+//        for (int i = 0; i < stackArr.length; i++) {
+//            stackSort[i][0] = stackArr[i];
+//            stackSort[i][1] = stackIndex[i];
+//        }
+//        Arrays.sort(stackSort, (a, b) -> (a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]));
+//        System.out.println("合并排序时间：" + (System.currentTimeMillis() - t2));
+//
+////        int maxIndex = stackSort[stackSort.length-1][1];
+//
+//        //根据index拆分stackArr,还原数组
+////        long t4 = System.currentTimeMillis();
+//        List<int[]> arrGroup = new ArrayList<>();
+//        int fromIndex = 0;
+//        for (int i = 0; i < stackSort[stackSort.length - 1][1] + 1; i++) {
+//            int[] arr = new int[map.get(i)];
+//            for (int j = 0; j < map.get(i); j++) {
+//                arr[j] = stackSort[fromIndex + j][0];
+//            }
+//            fromIndex += map.get(i);
+//            arrGroup.add(arr);
+//        }
+////        System.out.println("拆分数组时间："+(System.currentTimeMillis() - t4));
+//        return arrGroup;
+//    }
 
     //合并两个有序数组为一个
-    public static int[] merge(int[] nums1, int n1, int[] nums2, int n2) {
-        int p1 = n1 - 1, p2 = n2 - 1;
-        int tail = n1 + n2 - 1;
-        int cur;
-        while (p1 >= 0 || p2 >= 0) {
-            if (p1 == -1) {
-                cur = nums2[p2--];
-            } else if (p2 == -1) {
-                cur = nums1[p1--];
-            } else if (nums1[p1] > nums2[p2]) {
-                cur = nums1[p1--];
-            } else {
-                cur = nums2[p2--];
-            }
-            nums1[tail--] = cur;
-        }
-        return nums1;
-    }
+//    private static int[] merge(int[] nums1, int n1, int[] nums2, int n2) {
+//        int p1 = n1 - 1, p2 = n2 - 1;
+//        int tail = n1 + n2 - 1;
+//        int cur;
+//        while (p1 >= 0 || p2 >= 0) {
+//            if (p1 == -1) {
+//                cur = nums2[p2--];
+//            } else if (p2 == -1) {
+//                cur = nums1[p1--];
+//            } else if (nums1[p1] > nums2[p2]) {
+//                cur = nums1[p1--];
+//            } else {
+//                cur = nums2[p2--];
+//            }
+//            nums1[tail--] = cur;
+//        }
+//        return nums1;
+//    }
 
     @Data
     public static class Stack {
--- src/main/java/net/csibio/aird/testCompareStack.java
+++ src/main/java/net/csibio/aird/testCompareStack.java
@@ -1,6 +1,5 @@
 package net.csibio.aird;
 
-
 import net.csibio.aird.bean.BlockIndex;
 import net.csibio.aird.parser.DIAParser;
 import net.csibio.aird.util.CompressUtil;
@@ -9,28 +8,22 @@ import org.apache.lucene.util.RamUsageEstimator;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-public class testStack {
+public class testCompareStack {
     public static void main(String[] args) throws IOException {
-//        List<File> files = AirdScanUtil.scanIndexFiles("\\\\ProproNas\\ProproNAS\\data\\Aird\\DIA\\ThermoQE");
-//        if (files == null || files.size() == 0) {
-//            return;
-//        }
-//        for (File indexFile : files) {
-//            System.out.println(indexFile.getAbsolutePath());
-//        }whether git is ready.
-        String indexFilePath="/Users/jinyinwang/Documents/stackTestData/DIA/HYE110_TTOF6600_64var_lgillet_I160305_001.json";
+        String indexFilePath = "/Users/jinyinwang/Documents/stackTestData/DIA/HYE110_TTOF6600_64var_lgillet_I160305_001.json";
         DIAParser DIAParser = new DIAParser(indexFilePath);
         List<BlockIndex> swathIndexList = DIAParser.getAirdInfo().getIndexList();
-        String[][] record = new String[5][swathIndexList.size() + 1];
-        record[0][0] = "sizeOrigin";
-        record[1][0] = "size1";
-        record[2][0] = "encodeTime1";
-        record[3][0] = "size2";
-        record[4][0] = "encodeTime2";
+        long sizeOrigin = 0;
+        long tAird1 = 0;
+        long sizeAird1 = 0;
+        long[][] recordSize = new long[9][swathIndexList.size()];//记录2的幂次堆叠每个block的size
+        long[][] recordEncodeTime = new long[9][swathIndexList.size()];//记录2的幂次堆叠每个block的EncodeTime
 
-        for (int m = 0; m < 10; m++) {
+        for (int m = 0; m < swathIndexList.size(); m++) {
             BlockIndex index = swathIndexList.get(m);
             List<Float> rts = index.getRts();
             int mzNum = rts.size();
@@ -42,7 +35,7 @@ public class testStack {
                 arr = DIAParser.getSpectrumAsInteger(index, rts.get(i)).getMz();
                 mzGroup.add(arr);
             }
-            record[0][m + 1] = RamUsageEstimator.humanSizeOf(mzGroup);
+            sizeOrigin += RamUsageEstimator.sizeOf(mzGroup);
 //            String size1 = RamUsageEstimator.humanSizeOf(mzGroup);
 //            System.out.println("原数组：" + size1);
 
@@ -55,13 +48,12 @@ public class testStack {
                 t1 += (System.currentTimeMillis() - tempT);
                 comMZs.add(comMZ);
             }
-            record[1][m + 1] = RamUsageEstimator.humanSizeOf(comMZs);
-            record[2][m + 1] = t1 + "";
+            sizeAird1 = RamUsageEstimator.sizeOf(comMZs);
+            tAird1 = t1;
 //            String size2 = RamUsageEstimator.humanSizeOf(comMZs);
 //            System.out.println("一代：" + size2);
 //            System.out.println("一代压缩时间：" + t1);
-
-            for (int k = 8; k < 9; k++) {
+            for (int k = 2; k < 11; k++) {
                 long t2 = System.currentTimeMillis();
                 int arrNum = (int) Math.pow(2, k);
                 int groupNum = (mzNum - 1) / arrNum + 1;
@@ -78,22 +70,34 @@ public class testStack {
                 stackData2Rep.Stack stack = stackData2Rep.stackEncode(arrGroup, false);
                 stacks.add(stack);
                 long t3 = System.currentTimeMillis();
-                record[3][m + 1] = RamUsageEstimator.humanSizeOf(stacks);
-                record[4][m + 1] = (t3 - t2) + "";
+                recordSize[k - 2][m] = RamUsageEstimator.sizeOf(stacks);
+                recordEncodeTime[k - 2][m] = (t3 - t2);
 //                String size3 = RamUsageEstimator.humanSizeOf(stacks);
 //                System.out.println("二代：" + size3);
 //                System.out.println("二代压缩时间：" + (t3 - t2));
-                System.out.println("block"+m+" finished!");
+                System.out.println("block" + m + " finished!");
             }
         }
-        File file = new File(indexFilePath.replace("json","csv"));
+
+        File file = new File(indexFilePath.replace("json", "csv"));
         FileWriter out = new FileWriter(file);
-        for (int i = 0; i < swathIndexList.size(); i++) {
-            for (int j = 0; j < 5; j++) {
-                out.write(record[j][i]+",");
-            }
+        out.write("sizeOrigin" + "," + RamUsageEstimator.humanReadableUnits(sizeOrigin) + "\r\n");
+        out.write("k" + "," + "size" + "," + "encodeTime" + "\r\n");
+        out.write("0" + "," + RamUsageEstimator.humanReadableUnits(sizeAird1) + ",");
+        out.write(Long.toString(tAird1));
+        for (int i = 0; i < 9; i++) {
             out.write("\r\n");
+            long stackSize = 0;
+            long stackTime = 0;
+            for (int j = 0; j < swathIndexList.size(); j++) {
+                stackSize += recordSize[i][j];
+                stackTime += recordEncodeTime[i][j];
+            }
+            out.write((i + 2) + ",");
+            out.write(RamUsageEstimator.humanReadableUnits(stackSize) + ",");
+            out.write(Long.toString(stackTime));
         }
         out.close();
+
     }
 }
