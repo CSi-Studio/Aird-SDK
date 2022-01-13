ea67a1e5abf6a985237f38f0da07aff3d620b47d
admin <crystaljinyin@qq.com>
2022-01-13 18:11:17 +0800

    add MZmine result compare tool

---
+++ src/main/java/net/csibio/aird/mzmineResultCompare/PeakControl.java
@@ -0,0 +1,7 @@
+package net.csibio.aird.mzmineResultCompare;
+
+public interface PeakControl {
+    boolean equals(Object arg0, float tolerance);
+
+//    boolean containsValue(Object arg0, float tolerance);
+}
