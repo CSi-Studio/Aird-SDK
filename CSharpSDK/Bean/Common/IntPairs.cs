45cd06011222db434b3c2ca31c14a6529d60adba
lms <lms19880914@qq.com>
2023-03-28 16:09:44 +0800

    列存储代码编写

---
+++ CSharpSDK/Bean/Common/IntPairs.cs
@@ -0,0 +1,13 @@
+﻿namespace AirdSDK.Beans.Common;
+
+public class IntPairs
+{
+    public int[] left;
+    public int[] right;
+
+    public IntPairs(int[] left, int[] right)
+    {
+        this.left = left;
+        this.right = right;
+    }
+}
